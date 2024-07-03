788f5705dc6aeddf02dff348730a1bd156f2ab94
Some Cho <itssomicho@gmail.com>
2024-07-03 11:18:03 +0200

    0.0.3 - rework API
    - keywords :setup and :draw used in macro to differentiate blocks
    
    Previously, setup is a defmethod and so needs to come after the make macro.
    This is weird because setup happens before drawing. This new API makes that
    clear.
    
    - add dependency glkit

--- dripsy.asd
+++ dripsy.asd
@@ -2,11 +2,12 @@
   :description "Creative Coding Framework"
   :author "Somē Cho <itssomicho@gmail.com>"
   :license  "MIT"
-  :version "0.0.2"
+  :version "0.0.3"
   :serial t
   :depends-on (#:alexandria
                #:cl-opengl
-               #:cl-glfw3)
+               #:cl-glfw3
+               #:glkit)
   :pathname "src"
   :components ((:file "package")
                (:file "dripsy")
--- examples/gl/hello-triangle.lisp
+++ examples/gl/hello-triangle.lisp
@@ -3,20 +3,24 @@
 ;; the screen. It is usually the first example you do when learning openGL.
 
 (drip:make hello-triangle
+
+           :setup
+
+           (let ((*vbo* nil)
+                 (*vao* nil))
+             (setf *vbo* (gl:gen-buffer))
+             (gl:bind-buffer :array-buffer *vbo*)
+             (let ((array (drip:gen-gl-array-buffer (drip:triangle-points 200.0))))
+               (gl:buffer-data :array-buffer :static-draw array))
+             (setf *vao* (gl:gen-vertex-array))
+             (gl:bind-vertex-array *vao*)
+             (gl:enable-vertex-attrib-array 0)
+             (gl:vertex-attrib-pointer 0 3 :float :false 0 0))
+
+           :draw
+
            (gl:clear :color-buffer)
            (gl:draw-arrays :triangles 0 3))
-
-(defmethod drip:setup ((app hello-triangle))
-  (let ((*vbo* nil)
-        (*vao* nil))
-    (setf *vbo* (gl:gen-buffer))
-    (gl:bind-buffer :array-buffer *vbo*)
-    (let ((array (drip:gen-gl-array-buffer (drip:triangle-points))))
-      (gl:buffer-data :array-buffer :static-draw array))
-    (setf *vao* (gl:gen-vertex-array))
-    (gl:bind-vertex-array *vao*)
-    (gl:enable-vertex-attrib-array 0)
-    (gl:vertex-attrib-pointer 0 3 :float :false 0 0)))
 
 (make-instance 'hello-triangle :width 512
                                :height 512)
